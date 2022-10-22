7a5e45db3b209ea2715792577059f575476cc8d5
nikalex777 <777_nikolaev@mail.ru>
2022-10-22 23:05:18 +0300

    magic box

---
+++ src/MagicBox.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class MagicBox<T> {
+    protected int numberAndString;
+    protected T[] items;
+    protected int count;
+
+
+    public MagicBox(int numberAndString) {
+        this.numberAndString = numberAndString;
+        items = (T[]) new Object[numberAndString];
+    }
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                count++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("Коробка не полна и осталось ещё: " +
+                        (items.length - count) + " места.");
+            }
+        }
+
+        Random random = new Random();
+
+        int randomInt = random.nextInt(3);
+
+        return items[randomInt];
+    }
+}
